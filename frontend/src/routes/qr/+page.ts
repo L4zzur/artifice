8f72fee56c26adad4725ccfc15beca67c5ed3bee
Popov Nikita <popovnikita01@gmail.com>
2025-11-29 01:07:06 +0200

    fix(frontend): fix description qr page

--- frontend/src/routes/qr/+page.ts
+++ frontend/src/routes/qr/+page.ts
@@ -2,7 +2,7 @@ export const load = () => {
   return {
     meta: {
       title: "QR Code | Artifice",
-      description: "Scan and decode QR codes from images.",
+      description: "Generate and scan QR codes.",
     },
   };
 };
