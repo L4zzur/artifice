c91869060ae145a0acce361954053a89accb80b6
Popov Nikita <popovnikita01@gmail.com>
2025-11-28 22:26:37 +0200

    feature(backend): create hash info router

--- backend/api/v1/hash/__init__.py
+++ backend/api/v1/hash/__init__.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
 from .hash_generate import router as generate_router
+from .hash_info import router as info_router
 from .hash_verify import router as verify_router
 
 api_router = APIRouter(prefix="/hash", tags=["Hash Operations"])
 
 api_router.include_router(generate_router)
 api_router.include_router(verify_router)
+api_router.include_router(info_router)
